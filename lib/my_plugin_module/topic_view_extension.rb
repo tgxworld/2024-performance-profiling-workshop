fbc480159e6fa0d428c55e2489e3e91c504ed762
Alan Guo Xiang Tan <gxtan1990@gmail.com>
2024-09-19 11:26:09 +0800

    Moar bug fix

--- lib/my_plugin_module/topic_view_extension.rb
+++ lib/my_plugin_module/topic_view_extension.rb
@@ -5,7 +5,7 @@ module MyPluginModule
     extend ActiveSupport::Concern
 
     def unfiltered_posts
-      if @topic.id == ::MyPluginModule::SLOW_TOPIC_ID && @user && @user.skip_topic_view_sloth_virus
+      if @topic.id == ::MyPluginModule::SLOW_TOPIC_ID && @user && !@user.skip_topic_view_sloth_virus
         MyPluginModule::Fibonacci.fib(30)
       end
 
